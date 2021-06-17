2fdbaa82a17f3b58351566e0714a26a65a095956
Kira9112 <kira91_911@mail.ru>
2021-06-17 23:00:47 +0300

    Modified Java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int currentbalance = 100;
+        int currentBalance = 100;
         int refill = 1540;
         if (refill>1000) {refill=
             refill + refill/100 ;
         } else {
             refill=refill;
         }
-        int totalbalnce = currentbalance + refill;
+        int totalBalance = currentBalance + refill;
 
-        System.out.println(totalbalnce);
+        System.out.println(totalBalance);
     }
 }
